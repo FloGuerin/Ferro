1f3d788df0b712c2bcc5f83c153c2688be19c858
Florian Guerin <fguerin@isep.fr>
2013-06-20 16:36:15 +0200

    path

--- application/config/constants.php
+++ application/config/constants.php
@@ -36,7 +36,8 @@ define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
-define('URL','http://localhost/ferro/');
+
+define('URL',$_SERVER['DOCUMENT_ROOT']);
 define('RS',URL.'resources/rs-plugin/');
 define('IMG',URL.'resources/img/');
 define('CSS',URL.'resources/css/');
