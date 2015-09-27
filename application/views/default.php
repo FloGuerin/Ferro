c5d82bcf3f4b89bc38587823f533edadc206b35d
Florian Guerin <florianguerin@outlook.com>
2015-09-27 17:53:53 +0200

    aa

--- application/views/default.php
+++ application/views/default.php
@@ -17,7 +17,7 @@
         <link rel="stylesheet" href="<?php echo(CSS.'shortcodes.css'); ?>">
         <link rel="stylesheet" href="<?php echo(CSS.'responsive.css'); ?>">
         <link rel="stylesheet" href="<?php echo(CSS.'custom.css'); ?>">
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
         
         <script src="<?php echo(JS.'modernizr.js'); ?>"></script> <!-- Default JS -->
         <script src="<?php echo(JS.'jquery-1.9.1.min.js'); ?>"></script>
